b772cb8f581f824c39aec7b8258edf5d6e9cb578
Shinsaro <shinsaro69@gmail.com>
2023-09-18 15:14:15 +0700

    wip

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from "path"
-import { fileURLToPath } from "url"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  "base": "/",
   plugins: [react()],
-  resolve: {
-    alias: {
-      "~": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "src"),
-    },
-  },
 })
